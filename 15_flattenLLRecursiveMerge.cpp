7d2b81da39c8ae849cd36e3c60c2010a3fc6eb6a
RAmPAtel007 <codewizard0007@gmail.com>
2025-06-14 13:07:28 +0530

    gfg

---
+++ 15_flattenLLRecursiveMerge.cpp
@@ -0,0 +1,42 @@
+/* Node structure  used in the program
+
+struct Node{
+    int data;
+    struct Node * next;
+    struct Node * bottom;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+        bottom = NULL;
+    }
+
+};
+*/
+Node* merge(Node*a , Node*b){
+    if(!a)return b ; ///agar a empty hua toh b  ko return kr do 
+    if(!b) return a;
+    Node* ans= 0;
+    
+    if(a->data < b->data){
+        ans=a;
+        a->bottom = merge(a->bottom,b);
+        
+    }
+    else{
+        ans=b;
+        b->bottom = merge(a,b->bottom);
+    }
+    return ans;
+}
+class Solution {
+  public:
+    // Function which returns the  root of the flattened linked list.
+    Node *flatten(Node *root) {
+        // Your code here
+        if(!root) return 0;
+        Node* mergeLL = merge(root, flatten(root->next));
+        return mergeLL;
+        
+    }
+};
